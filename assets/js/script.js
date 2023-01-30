9c1eabbeea3774ce87123df05780e124c5a5eee1
Russ-S <rstanleym74@gmail.com>
2023-01-30 19:31:59 +0000

    JS: add basic structure for functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,36 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMutiplyQuestion() {
+
+}
+
+function displayDivideQuestion() {
+
+}
 
