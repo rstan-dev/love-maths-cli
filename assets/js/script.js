5bb6d8f083e6e7942bb3a438817afaa0331a4e90
Russ-S <rstanleym74@gmail.com>
2023-01-30 20:08:20 +0000

    JS: add event listeners to buttons and test with alert

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+// wait for the DOM to finish loading before running the game
+// get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+         button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+         });
+
+    }
+});
+
 function runGame() {
 
 }
@@ -34,3 +53,30 @@ function displayDivideQuestion() {
 
 }
 
+
+
+
+
+
+/* ------------------------------------------------------------------- */
+
+/* code development:
+--- Part One - checking that the event listeners are working using an alert()
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+         button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+         });
+
+    }
+});  
+
+---End of Part One */
